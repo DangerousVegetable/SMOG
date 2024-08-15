9b87cfa86b76c6ed290d7fc26ba8189ece32806d
DangerousVegetable <mukoseev.l@yandex.ru>
2024-08-15 11:48:04 +0300

    Add icon to the executable.

---
+++ smog/build.rs
@@ -0,0 +1,5 @@
+use embed_resource::NONE;
+
+fn main() {
+    embed_resource::compile("embed_icon.rc", NONE);
+}
--- smog/src/main.rs
+++ smog/src/main.rs
@@ -1,12 +1,13 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitWindows};
 
 mod ui;
 use network::client::GameClient;
 use packet_tools::game_packets::{GamePacket, PACKET_SIZE};
 use render::{RenderSimulationPlugin, SimulationCamera};
 use ui::{game::GamePlugin, lobby::LobbyPlugin, main_menu::MainMenuPlugin, over::WinScreenPlugin};
+use winit::window::Icon;
 
 mod network;
 mod controller;
@@ -39,6 +40,24 @@ fn display_error(commands: &mut Commands, next_state: &mut ResMut<NextState<Game
     next_state.set(GameState::Error)
 }
 
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+) {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open("assets/textures/icon.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+
+    for window in windows.windows.values() {
+        window.set_window_icon(Some(icon.clone()));
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -50,7 +69,7 @@ fn main() {
         }))
         .add_plugins(RenderSimulationPlugin)
         .add_plugins((MainMenuPlugin, LobbyPlugin, GamePlugin, WinScreenPlugin))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, set_window_icon))
         .insert_state(GameState::Menu)
         .run();
 }
